e25d1e58fdada6d299c63f3758db064cde6151b2
Okrekso <semikoseker@gmail.com>
2017-10-03 11:17:14 +0300

    правки дизигна

--- Droid/Resources/Resource.designer.cs
+++ Droid/Resources/Resource.designer.cs
@@ -44,6 +44,9 @@ namespace Playfie.Droid
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050004
+			public const int button1 = 2131034116;
+			
 			// aapt resource value: 0x7f050001
 			public const int editText1 = 2131034113;
 			
@@ -71,6 +74,9 @@ namespace Playfie.Droid
 			
 			// aapt resource value: 0x7f030000
 			public const int Main = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int registration = 2130903041;
 			
 			static Layout()
 			{
