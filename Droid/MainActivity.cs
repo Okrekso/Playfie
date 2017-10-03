39b08c19b2b577f5b3c2d9ea04ba719b58b64959
Okrekso <semikoseker@gmail.com>
2017-10-03 11:32:36 +0300

    добавил кнопку регистрации

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -1,22 +1,32 @@
 ﻿using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
 using Android.Widget;
 using Android.OS;
+using static Android.Views.View;
+using static Android.Locations.GpsStatus;
 
 namespace Playfie.Droid
 {
     [Activity(Label = "Playfie", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
-
-        protected override void OnCreate(Bundle savedInstanceState)
+        TextView t;
+        protected override void OnCreate(Bundle bundle)
         {
-            base.OnCreate(savedInstanceState);
+            base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+            t = (TextView)FindViewById(Resource.Id.registrText);
+            t.Click += goRegistr;
+        }
 
-            // Get our button from the layout resource,
-            // and attach an event to it
+        private void goRegistr(object sender, System.EventArgs e)
+        {
+            t = (TextView)FindViewById(Resource.Id.registrText);
+            SetContentView(Resource.Layout.registration);
         }
     }
 }
